c77dc274a5ea3e064f0d839e7e07e46d2707d7a6
vundi <vundi4773@gmail.com>
2024-07-27 18:57:30 +0300

    sql queries

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Mr.Chocolate',9,'M','Brown','dark',0)
+,('Rowdy',4,'M','black','light',1)
+
+,('Tabitha',12,'F','yellow','dark',0)
+
+,('Sergeant Brown',33,'M','green','dark',1)
+
+,('Melissa',16,'F','red','light',1)
+
+,('Grinch',10,'M','Blue','light',0)
+
+,('Wendy',11,'M','orange','dark',1)
+
+,(NULL,9,'M','indigo','dark',0)
